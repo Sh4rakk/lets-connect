9681dedebab306e9de0d21c6949655585705c981
Sharak <103329505+Sh4rakk@users.noreply.github.com>
2026-05-06 09:49:32 +0200

    Integrate Sentry for enhanced error tracking and warning capture in the application

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,17 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({
+    dsn: "https://7781e93829890a07687c40a296a1deba@o4511341734395904.ingest.de.sentry.io/4511341749076048",
+});
+
+const originalWarn = console.warn;
+console.warn = function(...args) {
+    Sentry.captureMessage(args.join(" "), "warning");
+    originalWarn.apply(console, args);
+};
 
 window.Alpine = Alpine;
 
